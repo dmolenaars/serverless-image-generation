0ce4899dc9b2d23b5444ced3daecaeeeb24dad3a
Daniel Molenaars <danielmolenaars@Admins-MacBook-Pro.local>
2024-03-22 17:12:06 +0100

    chore: refactor local environment check

--- src/app.py
+++ src/app.py
@@ -9,26 +9,29 @@ from alibabacloud_credentials.client import (
 from alibabacloud_oos20190601 import client, models as oos_models
 from alibabacloud_tea_openapi import models as open_api_models
 
-def is_function_compute_invocation():
-    return os.environ.get("FC_FUNCTION_NAME") is not None,
+def is_local_environment():
+    return os.environ.get("FC_FUNCTION_NAME") is None,
 
 def get_api_key():
-    function_compute_config = CredClientConfig(
-        type="sts",
-        access_key_id=os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_ID"),
-        access_key_secret=os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_SECRET"),
-        security_token=os.environ.get("ALIBABA_CLOUD_SECURITY_TOKEN"),
-    )
-    cred_client = CredClient(function_compute_config) if is_function_compute_invocation() else CredClient()
-    config = open_api_models.Config(
-        credential=cred_client, region_id="eu-central-1"
-    )
-    oos_client = client.Client(config=config)
-    get_secret_request = oos_models.GetSecretParameterRequest(
-        name="serverless-image-generation/dashscope-api-key", with_decryption=True
-    )
-    api_key = oos_client.get_secret_parameter(get_secret_request).body.parameter.value
-    return api_key
+    if is_local_environment():
+        return os.environ.get("DASHSCOPE_API_KEY")
+    else:
+        function_compute_config = CredClientConfig(
+            type="sts",
+            access_key_id=os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_ID"),
+            access_key_secret=os.environ.get("ALIBABA_CLOUD_ACCESS_KEY_SECRET"),
+            security_token=os.environ.get("ALIBABA_CLOUD_SECURITY_TOKEN"),
+        )
+        cred_client = CredClient(function_compute_config)
+        config = open_api_models.Config(
+            credential=cred_client, region_id="eu-central-1"
+        )
+        oos_client = client.Client(config=config)
+        get_secret_request = oos_models.GetSecretParameterRequest(
+            name="serverless-image-generation/dashscope-api-key", with_decryption=True
+        )
+        api_key = oos_client.get_secret_parameter(get_secret_request).body.parameter.value
+        return api_key
 
 
 def image_generator(description):
